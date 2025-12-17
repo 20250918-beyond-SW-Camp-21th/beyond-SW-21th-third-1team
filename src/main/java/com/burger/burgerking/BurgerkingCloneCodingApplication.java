9ce038119a6782031556a4afdd700ef983fc01b1
chaewookim <chaewookim2877@gmail.com>
2025-12-17 09:21:50 +0900

    initial commit

---
+++ src/main/java/com/burger/burgerking/BurgerkingCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.burger.burgerking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerkingCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerkingCloneCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burger/burgerking/BurgerkingCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burger.burgerking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerkingCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
